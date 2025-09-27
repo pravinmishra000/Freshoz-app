1596e7426e7a52b455b076494a76a30bcbd201c0
Pravin Mishra <pravinmishra000@gmail.com>
2025-09-27 16:34:22 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -3,7 +3,7 @@ import { getAuth, Auth } from 'firebase-admin/auth';
 import { credential } from 'firebase-admin';
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
