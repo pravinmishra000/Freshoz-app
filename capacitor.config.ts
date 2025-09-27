37e16ba91040dd1bced720ab9b0a8f8c8e3fac94
Pravin Mishra <pravinmishra000@gmail.com>
2025-09-27 17:56:18 +0000

    capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.freshoz.app',
+  appName: 'Freshoz-fresh',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
